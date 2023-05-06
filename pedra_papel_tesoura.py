f426068d4140932a5358378f76bebf753aa577dd
Wesley Freire <wesleyfreire1707@gmail.com>
2023-05-06 00:24:16 -0300

    atualização

--- pedra_papel_tesoura.py
+++ pedra_papel_tesoura.py
@@ -11,4 +11,23 @@ while True:
     num = random.randint(1,3)
     escolha_pc = opcoes[num]
 
-    
+    if sua_escolha == 'pedra' and escolha_pc == 'tesoura':
+        result = ganhou
+
+    elif sua_escolha == 'tesoura' and escolha_pc == 'papel':
+        result = ganhou
+
+    elif sua_escolha == 'papel' and escolha_pc == 'pedra':
+        result = ganhou
+
+    elif sua_escolha == escolha_pc:
+        result = 'empate'
+
+    else:
+        result = perdeu
+
+    print(f'{result}, vc escolheu {sua_escolha} e o pc escolheu{escolha_pc}')
+    continua = input('quer jogar novamente? (s/n)')
+
+    if continua != 's':
+        break
