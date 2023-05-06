5c0490b025a874f2f25656899cb0bc481fe0c94b
Wesley Freire <wesleyfreire1707@gmail.com>
2023-05-06 00:15:24 -0300

    atualização

--- pedra_papel_tesoura.py
+++ pedra_papel_tesoura.py
@@ -1,1 +1,14 @@
 import random
+
+opcoes = {1: 'pedra', 2: 'papel', 3: 'tesoura'}
+
+ganhou = 'vc ganhou'
+
+perdeu = 'vc perdeu'
+
+while True:
+    sua_escolha = input('pedra, papel ou tesoura')
+    num = random.randint(1,3)
+    escolha_pc = opcoes[num]
+
+    
